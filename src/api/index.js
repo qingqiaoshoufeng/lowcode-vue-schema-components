bc36d0061d0c57b2fc1872c97c76806a776c0133
chenyang2 <chenyang2@hxdi.com>
2024-03-18 15:59:41 +0800

    fix: v0.0.18

--- src/api/index.js
+++ src/api/index.js
@@ -4,7 +4,7 @@ const getSchemaByVersion = async (params) => {
 	const res = await axios.get(`http://10.13.4.153:3001/api/lowcode/page/schema`, {
 		params
 	});
-	return res
+	return res.data?.data
 }
 
 export {
